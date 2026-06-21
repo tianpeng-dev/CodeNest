24141a450b3b5a3f958fdabba9419dcea32b4cea
tianpeng <tjys@me.com>
2026-06-21 16:29:57 +0800

    chore: align backend with postgres

--- backend/src/main/java/com/codenest/backend/config/MybatisPlusConfig.java
+++ backend/src/main/java/com/codenest/backend/config/MybatisPlusConfig.java
@@ -13,7 +13,7 @@ public class MybatisPlusConfig {
   @Bean
   MybatisPlusInterceptor mybatisPlusInterceptor() {
     MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
     return interceptor;
   }
 }
